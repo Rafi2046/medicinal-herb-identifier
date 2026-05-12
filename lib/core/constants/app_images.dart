26b51acfad252426ffbe969e3e34cca44b46e256
Rafi2046 <ishmakrahat02@gmail.com>
2026-05-12 15:57:14 +0600

    up

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,82 +1,23 @@
 class AppImages {
-  // Splash Screen
-
-  static const String splashLogo = 'assets/app_logo/growbrandly.webp';
-
-  //homescreen
-  static const String bellIcon = 'assets/icons/bell_icon.webp';
-  static const String menuIcon = 'assets/icons/menu_icon.webp';
-
-  //all property icons
-  static const String locationPinIcon = "assets/icons/starIcon.webp";
-  static const String starIcon = "assets/icons/starIcon.webp";
-
-  //property_screen
-  static const String arrowbackIcon = 'assets/icons/arrowbackIcon.webp';
-
-  static const String arrowPreviousIcon =
-      'assets/icons/arrow_previous_arrow.webp';
-  static const String arrowNextIcon = 'assets/icons/arrow_next_white.webp';
-  static const String dropdownIcon = 'assets/icons/dropdown_icon.webp';
-  static const String occupiedIcon = 'assets/icons/occupied_icon.webp';
-  static const String propertyIcon = 'assets/icons/property_icon.webp';
-  static const String searchIcon = 'assets/icons/search_icon.webp';
-  static const String vacancyIcon = 'assets/icons/vacancy_icon.webp';
-  static const String cityImage = 'assets/dummy_images/city_images.webp';
-  static const String landIcon = 'assets/icons/land_icon.webp';
-  static const String multiplePropertyIcon =
-      'assets/icons/multiple_property.webp';
-  static const String singlePropertyIcon = 'assets/icons/single_property.webp';
-  static const String allIcon = 'assets/icons/all_icon.webp';
-  static const String addIcon = 'assets/icons/add_icon.webp';
-  static const String saveIcon = 'assets/icons/save_icon.webp';
-  static const String attachmentIcon = 'assets/icons/attachment_icon.webp';
-
-  //bottom nav updateIcons
-
-  static const String bookingIcon = 'assets/icons/bookings_icon.png';
-  static const String homeIcon = 'assets/icons/home.webp';
-  static const String exploreIcon = 'assets/icons/explore_icon.webp';
-  static const String profileIcon = 'assets/icons/profile_icon.webp';
-
-  // bottom nav icons
-
-  static const String calendarIcon = 'assets/icons/calender_icon.webp';
-  static const String idIcon = 'assets/icons/id_icon.webp';
-
-
-  //notification icons
-  static const String notificationIcon = "assets/icons/notification_icon.webp";
-
-
-  static const String profilePicture = 'assets/dummy_images/profile_photo.png';
-
-
-
-  //payments screen
-
-  static const String logo = 'assets/app_logo/Logo.webp';
-  static const String printerIcon = 'assets/icons/printer_icon.webp';
-  static const String submittedIcon = 'assets/icons/submitted_icon.webp';
-  static const String editPen = 'assets/icons/edit_pen.webp';
-
-
-
-
-
-//herb
+  //herb
 
   static const String cardImage = 'assets/background_image/card_image.webp';
   static const String uploadIcon = 'assets/icons/upload_icon.webp';
   static const String cameraIcon = 'assets/icons/camera_icon.webp';
-  static const String containerImage = 'assets/background_image/containerImage.webp';
-  static const String containerWhite = 'assets/background_image/cointainerWhite.webp';
-  static const String historyIcon= 'assets/icons/upload_icon.webp';
+  static const String containerImage =
+      'assets/background_image/containerImage.webp';
+  static const String containerWhite =
+      'assets/background_image/cointainerWhite.webp';
+  static const String historyIcon = 'assets/icons/upload_icon.webp';
   static const String favoriteIcon = 'assets/icons/favorite_icon.webp';
   static const String whiteColor = 'assets/background_image/white.webp';
 
 
 
+
+
+
+  static const String splashImage = 'assets/app_logo/splash_image.webp';
 
 
 }
--- lib/features/splash/presentations/page/splash_screen.dart
+++ lib/features/splash/presentations/page/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:medical_herb/core/constants/app_spacing.dart';
+import 'package:medical_herb/core/constants/app_images.dart';
+
 import 'package:medical_herb/core/constants/app_text_styles.dart';
 import 'package:medical_herb/features/main/presentation/pages/main_screen.dart';
 
@@ -39,37 +40,11 @@ class _SplashScreenState extends State<SplashScreen> {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Image.asset(
-              'assets/background_image/shape.webp',
+          Center(child: Text('Medical Herb', style: AppTextStyles.heading2)),
+          SizedBox.expand(
+            child: Image(
+              image: AssetImage(AppImages.splashImage),
               fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return const SizedBox();
-              },
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/app_logo/logo.png',
-                  height: AppSpacing.h100,
-                  fit: BoxFit.contain,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Icon(
-                      Icons.local_hospital,
-                      size: 100,
-                      color: Colors.green[700],
-                    );
-                  },
-                ),
-                const SizedBox(height: 16),
-                Text('HerbGen', style: AppTextStyles.splashText),
-              ],
             ),
           ),
         ],
