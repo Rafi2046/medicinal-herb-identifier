365624eb10c884ee25d87230f1c78c3d82c301b9
Rafi2046 <ishmakrahat02@gmail.com>
2026-05-17 00:51:12 +0600

    updated

--- lib/features/screens/upload_screen.dart
+++ lib/features/screens/upload_screen.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:medical_herb/core/constants/app_text_styles.dart';
+import 'package:medical_herb/core/theme/app_colors.dart';
+import 'package:medical_herb/features/common_widgets/app_bar_widget.dart';
+
+import '../common_widgets/topbar_widget.dart';
+
 class UploadScreen extends StatelessWidget {
   const UploadScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.white,
-        ),
+      body: Column(
+        children: [
+          AppBarWidget(title: 'Details Screen'),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(color: AppColors.borderColors),
+              ),
+              child: SizedBox(
+                height: 135,
+                width: double.infinity,
+
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'IDENTIFIED AS ',
+                            style: AppTextStyles.detailsText,
+                          ),
+                          Text('Akando ', style: AppTextStyles.herbName),
+                          Text(
+                            'Origanum vulgare ',
+                            style: AppTextStyles.detailsTextT,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: AppColors.herbColorsName,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Text(
+                            'Lamiaceae',
+                            style: AppTextStyles.herbNameScientific,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
