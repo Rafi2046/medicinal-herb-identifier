511ca9661c4ceeab6acbd7641f62897e1dbbf965
Rafi2046 <ishmakrahat02@gmail.com>
2026-05-18 12:15:29 +0600

    updated

--- lib/features/screens/explore_screen.dart
+++ lib/features/screens/explore_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:medical_herb/core/constants/app_images.dart';
 import 'package:medical_herb/features/screens/widgets/explore_screen_widget.dart';
+
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Column(
-        children: [
-          ExploreScreenWidget()
-        ],
+    return Scaffold(
+      body: ListView.separated(
+        padding: const EdgeInsets.all(20),
+        itemCount: 10,
+        separatorBuilder: (_, __) => const SizedBox(height: 12),
+        itemBuilder: (context, index) {
+          return ExploreScreenWidget(
+            herbName: 'Moringa',
+            scientificName: 'Ocimum basilicum',
+            imagePath: AppImages.exploreImage,
+          );
+        },
       ),
     );
   }
--- lib/features/screens/widgets/explore_screen_widget.dart
+++ lib/features/screens/widgets/explore_screen_widget.dart
@@ -3,9 +3,19 @@ import 'package:medical_herb/core/constants/app_images.dart';
 import 'package:medical_herb/core/constants/app_spacing.dart';
 import 'package:medical_herb/core/constants/app_text_styles.dart';
 import 'package:medical_herb/core/theme/app_colors.dart';
+import 'package:medical_herb/features/screens/herb_full_details_screen.dart';
 
 class ExploreScreenWidget extends StatelessWidget {
-  const ExploreScreenWidget({super.key});
+  final String? herbName;
+  final String? scientificName;
+  final String? imagePath;
+
+  const ExploreScreenWidget({
+    super.key,
+    this.herbName,
+    this.scientificName,
+    this.imagePath,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,52 +24,47 @@ class ExploreScreenWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         side: BorderSide(color: AppColors.borderColor),
       ),
-      child: Column(
-        children: [
-          Container(
-            height: 140,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: AppColors.borderColor),
+                ),
+                child: Image(
+                  image: AssetImage(imagePath ?? AppImages.exploreImage),
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Row(
-                spacing: AppSpacing.s8,
+            const SizedBox(width: AppSpacing.s8),
+            Expanded(
+              child: Column(
+                spacing: AppSpacing.s4,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 2,
-                          color: AppColors.borderColor,
-                        ),
-                      ),
-                      child: Image(
-                        image: AssetImage(AppImages.exploreImage),
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-
-                  Column(
-                    spacing: AppSpacing.s4,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Moringa', style: AppTextStyles.confidenceName),
-                      Text('Ocimum basilicum', style: AppTextStyles.desText),
-                    ],
-                  ),
-
-                  Expanded(child: Icon(Icons.arrow_forward_ios, size: 16,color: AppColors.borderColor,)),
+                  Text(herbName ?? 'Moringa', style: AppTextStyles.confidenceName),
+                  Text(scientificName ?? 'Ocimum basilicum', style: AppTextStyles.desText),
                 ],
               ),
             ),
-          ),
-        ],
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HerbFullDetailsScreen()),
+                );
+              },
+              child: Icon(Icons.arrow_forward_ios, size: 16, color: AppColors.borderColor),
+            ),
+          ],
+        ),
       ),
     );
   }
