8ea20a3dd39bf8dfc066099e5bb57f1bd9086409
Rafi2046 <ishmakrahat02@gmail.com>
2026-05-18 12:27:21 +0600

    updated

--- lib/features/screens/explore_screen.dart
+++ lib/features/screens/explore_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medical_herb/core/constants/app_images.dart';
+import 'package:medical_herb/core/constants/app_text_styles.dart';
 import 'package:medical_herb/features/screens/widgets/explore_screen_widget.dart';
 
 class ExploreScreen extends StatelessWidget {
@@ -8,17 +9,31 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-        padding: const EdgeInsets.all(20),
-        itemCount: 10,
-        separatorBuilder: (_, __) => const SizedBox(height: 12),
-        itemBuilder: (context, index) {
-          return ExploreScreenWidget(
-            herbName: 'Moringa',
-            scientificName: 'Ocimum basilicum',
-            imagePath: AppImages.exploreImage,
-          );
-        },
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            spacing: 8,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Explore Herbs', style: AppTextStyles.confidenceName),
+
+              Expanded(
+                child: ListView.separated(
+                  itemCount: 10,
+                  separatorBuilder: (_, __) => const SizedBox(height: 8),
+                  itemBuilder: (context, index) {
+                    return ExploreScreenWidget(
+                      herbName: 'Moringa',
+                      scientificName: 'Moringa oleifera',
+                      imagePath: AppImages.exploreImage,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
