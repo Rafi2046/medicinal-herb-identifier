d48b7c5646f44dcdb8c058fa66a55c55c11c0dea
Rafi2046 <ishmakrahat02@gmail.com>
2026-05-19 15:29:54 +0600

    updated

--- lib/features/screens/main_screen.dart
+++ lib/features/screens/main_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:medical_herb/features/bottom_nav/bottom_nav_screen.dart';
-import 'package:medical_herb/features/main/presentation/widgets/main_app_top_bar.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const MainAppTopBar(),
-      body: const BottomNavScreen(),
+    return const Scaffold(
+      body: BottomNavScreen(),
     );
   }
 }
