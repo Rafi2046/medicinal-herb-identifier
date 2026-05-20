87c753a52b2fef7c2f86bf88ad9f672e3c1bec97
Rafi2046 <ishmakrahat02@gmail.com>
2026-05-20 14:53:09 +0600

    updated

--- lib/features/screens/scan_screen.dart
+++ lib/features/screens/scan_screen.dart
@@ -1,9 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:medical_herb/features/bottom_nav/bottom_nav_screen.dart';
 import 'package:medical_herb/core/constants/app_text_styles.dart';
 
-class ScanScreen extends StatelessWidget {
+class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
+
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  final ImagePicker _picker = ImagePicker();
+  bool _hasOpenedCamera = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!_hasOpenedCamera) {
+        _hasOpenedCamera = true;
+        _openCamera();
+      }
+    });
+  }
+
+  Future<void> _openCamera() async {
+    try {
+      final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+      if (image != null) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Image captured: ${image.path}')),
+          );
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Camera error: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +57,7 @@ class ScanScreen extends StatelessWidget {
             children: [
               const Icon(Icons.camera_alt_outlined, size: 64, color: Color(0xFF2D9E61)),
               const SizedBox(height: 16),
-              Text('Scan', style: AppTextStyles.heading2),
+              Text('Opening Camera...', style: AppTextStyles.heading2),
               const SizedBox(height: 8),
               Text('Point the camera at a herb to identify it.', style: AppTextStyles.body3),
             ],
