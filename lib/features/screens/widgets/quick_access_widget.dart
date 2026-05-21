cc5d7afac4c4e5be04af5067c1586c389a9d1eea
Rafi2046 <onestappdev103@gmail.com>
2026-05-22 01:03:44 +0600

    updated

--- lib/features/screens/widgets/quick_access_widget.dart
+++ lib/features/screens/widgets/quick_access_widget.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'package:medical_herb/core/constants/app_images.dart';
 import 'package:medical_herb/core/constants/app_spacing.dart';
 import 'package:medical_herb/core/constants/app_text_styles.dart';
+import 'package:medical_herb/core/providers/favorites_provider.dart';
 import 'package:medical_herb/core/theme/app_colors.dart';
+import 'package:medical_herb/features/screens/saved_screen.dart';
 
 class QuickAccessWidget extends StatelessWidget {
-  final VoidCallback? onPressed;
+  final VoidCallback? onHistoryPressed;
 
-  const QuickAccessWidget({super.key, this.onPressed});
+  const QuickAccessWidget({super.key, this.onHistoryPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -16,53 +18,66 @@ class QuickAccessWidget extends StatelessWidget {
       spacing: 8,
       children: [
         Expanded(
-          child: Container(
-            height: 80,
-            clipBehavior: Clip.antiAlias,
-
-            decoration: BoxDecoration(
-              border: Border.all(width: 2, color: Colors.teal),
-              borderRadius: BorderRadius.circular(20),
-              color: AppColors.white,
-            ),
-
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                spacing: AppSpacing.s8,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image(
-                    image: AssetImage(AppImages.favoriteIcon),
-                    width: AppSpacing.w40,
-                    height: AppSpacing.h40,
+          child: Consumer<FavoritesProvider>(
+            builder: (context, favs, _) {
+              final count = favs.items.length;
+              return Material(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(20),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const SavedScreen(showBackButton: true)),
                   ),
-
-                  Column(
-                    spacing: AppSpacing.s2,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Favorites', style: AppTextStyles.uploadText),
-
-                      SizedBox(height: AppSpacing.s4),
-
-                      Text('0 saved herbs', style: AppTextStyles.quickSubTile),
-                    ],
+                  child: Container(
+                    height: 80,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.teal),
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        spacing: AppSpacing.s8,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image(
+                            image: AssetImage(AppImages.favoriteIcon),
+                            width: AppSpacing.w40,
+                            height: AppSpacing.h40,
+                          ),
+                          Column(
+                            spacing: AppSpacing.s2,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Favorites', style: AppTextStyles.uploadText),
+                              SizedBox(height: AppSpacing.s4),
+                              Text(
+                                count == 0 ? 'No saved herbs' : '$count saved herb${count > 1 ? 's' : ''}',
+                                style: AppTextStyles.quickSubTile,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
         ),
-
         Expanded(
           child: Material(
             color: Colors.transparent,
             borderRadius: BorderRadius.circular(20),
             child: InkWell(
               borderRadius: BorderRadius.circular(20),
-              onTap: onPressed,
+              onTap: onHistoryPressed,
               child: Container(
                 height: 80,
                 clipBehavior: Clip.antiAlias,
@@ -71,7 +86,6 @@ class QuickAccessWidget extends StatelessWidget {
                   border: Border.all(width: 2, color: Colors.teal),
                   borderRadius: BorderRadius.circular(20),
                 ),
-
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Row(
@@ -82,15 +96,12 @@ class QuickAccessWidget extends StatelessWidget {
                         width: AppSpacing.w40,
                         height: AppSpacing.h40,
                       ),
-
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text('History', style: AppTextStyles.uploadText),
-
                           SizedBox(height: AppSpacing.s4),
-
                           Text(
                             'Recent scans',
                             style: AppTextStyles.quickSubTile,
