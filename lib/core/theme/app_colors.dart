64149806f2d1528010023bd19dd5a10b65517e66
Rafi2046 <onestappdev103@gmail.com>
2026-05-29 03:06:16 +0600

    updated

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-
   static const Color primaryColor = Color(0xFF3bbcfa);
   static const Color secondaryColor = Color(0xFFF28A21); // herb
   static const Color borderColor = Color(0xFFD6E2EF);
 
   static const Color herbProgressColor = Color(0xFF28c55e); // herb
   static const Color progressColor = Color(0xFF28c55e); // herb
-
-
 
   static const Color black = Color(0xFF161F2C);
   static const Color black60 = Color(0xFF545C66);
@@ -20,26 +17,23 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color backgroundColor = Color(0xFFF2F4F5);
   static const Color green = Color(0xFF4b7c60); // herb
-    static const Color detailsText = Color(0xFFc1d1c6); // herb
-    static const Color herbName = Color(0xFF14532c); // herb
-    static const Color characterName = Color(0xFF70e59c); // herb
+  static const Color detailsText = Color(0xFFc1d1c6); // herb
+  static const Color herbName = Color(0xFF14532c); // herb
+  static const Color characterName = Color(0xFF70e59c); // herb
 
   static const Color keyTraits = Color(0xFFa1bba9); // herb
   static const Color desText = Color(0xFF4b7c59); // herb
   static const Color savedText = Color(0xFFa6c0ae); // herb
 
-
-
-    static const Color herbScientific = Color(0xFF4b7c59); // herb
-    static const Color  borderColors = Color(0xFFe2f0e5); // herb
-    static const Color  herbColorsName = Color(0xFFfef7e7); // herb
-    static const Color  herbProgressColors = Color(0xFFe2f7eb); // herb
+  static const Color herbScientific = Color(0xFF4b7c59); // herb
+  static const Color borderColors = Color(0xFFe2f0e5); // herb
+  static const Color herbColorsName = Color(0xFFfef7e7); // herb
+  static const Color herbProgressColors = Color(0xFFe2f7eb); // herb
 
   static const Color containerColorRed = Color(0xFFFBEBEC);
   static const Color titleColorXL = Color(0xFFFBEBEC);
   static const Color previousButtonColor = Color(0x1A087C7C);
-  static const Color activeColor = Color(0xFFa0bba8);  // herb
+  static const Color activeColor = Color(0xFFa0bba8); // herb
   static const Color inactiveColor = Color(0xFFF26423);
   static const Color activeColor2 = Color(0xFFE6F2F2);
-
 }
