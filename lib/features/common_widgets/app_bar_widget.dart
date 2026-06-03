2dc0703b12517768bd81df060b663d6b43434da9
Rafi2046 <ishmakrahat02@gmail.com>
2026-06-03 12:21:02 +0600

    ui updated

--- lib/features/common_widgets/app_bar_widget.dart
+++ lib/features/common_widgets/app_bar_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:medical_herb/core/constants/app_text_styles.dart';
 import 'package:medical_herb/core/theme/app_colors.dart';
-import 'package:medical_herb/features/screens/widgets/screen_guide_bottom_sheet.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   final String? title;
@@ -94,19 +93,7 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
           ],
         ),
 
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.help_outline_rounded,
-              color: isDark ? Colors.white : AppColors.herbName,
-              size: 26,
-            ),
-            onPressed: () {
-              ScanGuideBottomSheet.show(context);
-            },
-          ),
-          const SizedBox(width: 8),
-        ],
+        actions: const [SizedBox(width: 8)],
       ),
     );
   }
